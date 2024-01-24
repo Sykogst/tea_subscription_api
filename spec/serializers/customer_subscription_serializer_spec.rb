e4b8d740defc9e9205b6311a6f89c3ba7ef54e97
Sam Tran <samuelttran@gmail.com>
2024-01-24 15:41:22 -0700

    Correct serializer test for customer_subscription

--- spec/serializers/customer_subscription_serializer_spec.rb
+++ spec/serializers/customer_subscription_serializer_spec.rb
@@ -5,21 +5,28 @@ RSpec.describe CustomerSubscriptionSerializer, type: :serializer do
     it 'serializes customer subscription data' do
       customer = create(:customer)
       subscription = create(:subscription)
-      customer_subscription_payload = {
-        customer_id: customer.id,
-        subscription_id: subscription.id
-      }
-      headers = { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
-  
-      post '/api/v0/customer_subscriptions', headers: headers, params: JSON.generate(customer_subscription: customer_subscription_payload)
-      created_customer_subscription = CustomerSubscription.last
-      serialized_data = CustomerSubscriptionSerializer.new(created_customer_subscription)
+      customer_subscription = create(:customer_subscription, customer: customer, subscription: subscription)
 
-      expect(serialized_data).to have_key("customer_id")
-      expect(serialized_data["customer_id"]).to eq(customer_subscription_payload[:customer_id])
+      serialized_cs = CustomerSubscriptionSerializer.new(customer_subscription).as_json
+      expect(serialized_cs).to have_key('data')
 
-      expect(serialized_data).to have_key("subscription_id")
-      expect(serialized_data["subscription_id"]).to eq(customer_subscription_payload[:subscription_id])
+      serialized_data = serialized_cs['data']
+      expect(serialized_data).to have_key('id')
+      expect(serialized_data['id']).to eq(customer_subscription.id.to_s)
+      expect(serialized_data).to have_key('type')
+      expect(serialized_data['type']).to eq('customer_subscription')
+      expect(serialized_data).to have_key('attributes')
+      expect(serialized_data).to have_key('relationships')
+
+      serialized_attributes = serialized_data['attributes']
+      expect(serialized_attributes).to have_key('customer_id')
+      expect(serialized_attributes['customer_id']).to eq(customer.id)
+      expect(serialized_attributes).to have_key('subscription_id')
+      expect(serialized_attributes['subscription_id']).to eq(subscription.id)
+
+      serialized_relationships = serialized_data['relationships']
+      expect(serialized_relationships).to have_key('customer')    
+      expect(serialized_relationships).to have_key('subscription')    
     end
   end
 end
